4c2dfa6387278cf399998ce3394a69a6cee5b31b
ItzCrαft <Tundukartem2397@gmail.com>
2026-09-01 19:12:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const exitButton = document.getElementById("exitButton");
+
+exitButton.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    alert("Здесь можно добавить своё действие для кнопки «ВЫХОД».");
+
+});
